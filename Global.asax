7937ffd39c6f2e4aa3402efaee6c6eb5e3a568bb
Culeac <124127335+Culeac@users.noreply.github.com>
2023-02-22 14:35:52 +0200

    Add files via upload

--- Global.asax
+++ Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webproject.Global" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MRSUTWEB.MvcApplication" Language="C#" %>
--- Global.asax.cs
+++ Global.asax.cs
@@ -1,21 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
-namespace webproject
+namespace MRSUTWEB
 {
-    public class Global : HttpApplication
+    public class MvcApplication : System.Web.HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        protected void Application_Start()
         {
-            // Код, выполняемый при запуске приложения
-           AreaRegistration.RegisterAllAreas();
-           RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();  //21313
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
