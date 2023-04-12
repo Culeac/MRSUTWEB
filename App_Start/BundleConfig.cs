3cfac0fcf930adfb515c48af849ba35bc9bae0c2
Liviu <liviu.cordonean@isa.utm.md>
2023-04-12 12:52:16 +0300

    imported template

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -10,9 +10,24 @@ namespace WebApplication1.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                      "~/Content/bootstrap.min.css"));
+
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                       "~/Content/main.css",
                       "~/Content/noscript.css"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                    "~/Scripts/Template.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(
+                       "~/Content/bootstrap.min.js"
+                       ));
+
+            bundles.Add(new StyleBundle("~/bundles/font-awesome").Include(
+                       "~/Content/font-awesome/css/font-awesome.css",
+                       "~/Content/font-awesome/css/font-awesome.min.css",
+                       "~/Content/font-awesome/css/main.css",
+                       "~/Content/font-awesome/css/noscript.css"));
         }
     }
 }
