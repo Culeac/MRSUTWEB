07965e3a9ae4cfe66d8e91fc17fb4332bff0a065
Liviu <liviu.cordonean@isa.utm.md>
2023-05-27 10:23:52 +0300

    Adaugare pagina produse

---
+++ Models/Product/NewProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace WebApplication1.Models.Product
+{
+    public class NewProduct
+    {
+        [Display(Name = "Product name")]
+        [Required(ErrorMessage = "Numele produsului este obligatoriu")]
+        public string Name { get; set; }
+
+        [Display(Name = "Product description")]
+        [Required(ErrorMessage = "Descrierea produsului este obligatorie")]
+        public string Description { get; set; }
+
+        [Display(Name = "Product price")]
+        [Required(ErrorMessage = "Pretul produsului este obligatoriu")]
+        public int Price { get; set; }
+
+        [Display(Name = "Product image")]
+        public string ImageLink { get; set; }
+    }
+}
